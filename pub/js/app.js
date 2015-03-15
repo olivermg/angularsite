c4345997e9f8f3ce7eba2fc7901a2e600d486b36
Oliver Wegner <void1976@gmail.com>
2015-03-15 21:13:20 +0100

    added routing

--- pub/js/app.js
+++ pub/js/app.js
@@ -1,13 +1,36 @@
 
 var owApp = angular.module('owApp', ['ngRoute']);
 
-owApp.controller('UserCtrl', function($scope) {
-	$scope.users = [
-	{ name: 'Oliver' },
-	{ name: 'Hans' },
-	{ name: 'Foo' },
-	{ name: 'John Doe' },
-	{ name: 'Otto' }
-	];
-});
+owApp.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+  .when('/body', {
+    controller: 'BodyCtrl',
+    templateUrl: 'root.html'
+  })
+  .when('/settings', {
+    controller: 'SettingsCtrl',
+    templateUrl: 'settings.html'
+  })
+  .otherwise({ redirectTo: '/' });
+}]);
 
+owApp.controller('BodyCtrl', ['$scope', function ($scope) {
+  $scope.content = { value: 'aaaaaaaaaaaaaaaaaaaa' };
+}]);
+
+owApp.controller('UserCtrl', ['$scope', function ($scope) {
+  $scope.users = [
+    { name: 'Oliver' },
+    { name: 'Hans' },
+    { name: 'Foo' },
+    { name: 'John Doe' },
+    { name: 'Otto' }
+  ];
+}]);
+
+owApp.controller('SettingsCtrl', ['$scope', function ($scope) {
+  $scope.settings = [
+    { name: 'settingA' },
+    { name: 'settingB' }
+  ];
+}]);
