4e68c4bf88c496af393da8549ed3919af070c375
Oliver Wegner <void1976@gmail.com>
2015-03-19 00:04:33 +0100

    added routes

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var app = express();
+
 app.get('/', function(req, res) {
-	res.send('Hello World!');
+	res.render('index.jade');
+});
+app.get('/welcome', function(req, res) {
+	res.render('welcome.jade');
+});
+app.get('/app', function(req, res) {
+	res.render('app.jade');
 });
 app.get('/main', function(req, res) {
-	res.render('main.html');
+	res.render('main.jade');
 });
+app.get('/settings', function(req, res) {
+	res.render('settings.jade');
+});
+
 var server = app.listen(8081, function() {
 	console.log("listening...");
 });
