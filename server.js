95fc92e6f314971b659d2e233118ab0c56dbf868
Oliver Wegner <void1976@gmail.com>
2015-03-21 01:44:12 +0100

    adjusted redirect

--- server.js
+++ server.js
@@ -10,26 +10,12 @@ appRouter.get('/:action', function(req, res) {
 	var action = req.params.action;
 	res.render(action + '.jade');
 });
-/*
-appRouter.get('/welcome', function(req, res) {
-	res.render('welcome.jade');
-});
-appRouter.get('/app', function(req, res) {
-	res.render('app.jade');
-});
-appRouter.get('/main', function(req, res) {
-	res.render('main.jade');
-});
-appRouter.get('/settings', function(req, res) {
-	res.render('settings.jade');
-});
-*/
 
 
 app.use('/pub', staticRouter);
 app.use('/', appRouter);
 app.get('/', function(req, res) {
-	res.redirect(301, '/index');
+	res.render('index.jade');
 });
 
 
