c15eb5b0f081d143e70798e57ef8f9fb1d68cd23
Oliver Wegner <void1976@gmail.com>
2015-03-24 22:23:33 +0100

    added passport & express-session to express app

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var passport = require('passport');
+var expressSession = require('express-session');
 var bodyParser = require('body-parser');
 
 var staticRouter = express.static(__dirname + '/pub');
@@ -22,6 +24,9 @@ apiRouter.get('/:action', function(req, res) {
 });
 
 var app = express();
+app.use(expressSession({secret: 'mySecret'}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/pub', staticRouter);
